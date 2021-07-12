d4c915db7d7ba917f84095d6094e939b3dff0d82
Aaron Strong <aaronspong1212@gmail.com>
2021-07-12 18:44:16 +0100

    Replace BTC route verification with LTC

--- src/routes/electrum.php
+++ src/routes/electrum.php
@@ -11,26 +11,26 @@ if (config('electrum.web_interface.enabled', false)) {
 
             Route::get('history', 'AraneaDev\Electrum\App\Api\HistoryController@index');
             Route::get('history/{address}', 'AraneaDev\Electrum\App\Api\HistoryController@show')
-                ->where('address', '^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$');
+                ->where('address', '^(ltc1|[LM])[a-zA-HJ-NP-Z0-9]{26,40}$');
             Route::get('history/tx/{txid}', 'AraneaDev\Electrum\App\Api\HistoryController@details');
 
             Route::get('addresses', 'AraneaDev\Electrum\App\Api\AddressController@index');
             Route::get('addresses/unused', 'AraneaDev\Electrum\App\Api\AddressController@unused');
             Route::get('addresses/is_mine{address}', 'AraneaDev\Electrum\App\Api\AddressController@is_mine')
-                ->where('address', '^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$');
+                ->where('address', '^(ltc1|[LM])[a-zA-HJ-NP-Z0-9]{26,40}$');
             Route::get('addresses/{address}', 'AraneaDev\Electrum\App\Api\AddressController@check')
-                ->where('address', '^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$');
+                ->where('address', '^(ltc1|[LM])[a-zA-HJ-NP-Z0-9]{26,40}$');
 
             Route::prefix('requests')->group(function () {
                 Route::get('/', 'AraneaDev\Electrum\App\Api\RequestsController@index');
                 Route::get('{address}', 'AraneaDev\Electrum\App\Api\RequestsController@show')
-                    ->where('address', '^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$');
+                    ->where('address', '^(ltc1|[LM])[a-zA-HJ-NP-Z0-9]{26,40}$');
 
                 Route::post('/', 'AraneaDev\Electrum\App\Api\RequestsController@create');
 
                 Route::delete('/', 'AraneaDev\Electrum\App\Api\RequestsController@clear');
                 Route::delete('{address}', 'AraneaDev\Electrum\App\Api\RequestsController@destroy')
-                    ->where('address', '^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$');
+                    ->where('address', '^(ltc1|[LM])[a-zA-HJ-NP-Z0-9]{26,40}$');
             });
         });
     });
